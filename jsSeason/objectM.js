2dfe6e39beae2c7bd1f146822bba15c98a9c0183
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2025-02-01 14:31:19 +0530

    bind

--- jsSeason/objectM.js
+++ jsSeason/objectM.js
@@ -13,5 +13,11 @@ let printName = function(city , country){
 // printName.call(person1,["Pune" , "India"])
 // printName.call(person2,["Bengaluru" , "India"])
 
-printName.apply(person1,["Pune" , "India"])
-printName.apply(person2,["Bengaluru" , "India"])
+// printName.apply(person1,["Pune" , "India"])
+// printName.apply(person2,["Bengaluru" , "India"])
+
+// let bindMethod1 = printName.bind(person1 , "Delhi" , "India")
+// bindMethod1()
+
+let bindMethod2 = printName.bind(person2 , "Haryana" , "India")
+bindMethod2()
