6ef50b53219608320997d9f8ef77d9c6d32b4d00
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2025-02-01 14:24:19 +0530

    call

---
+++ jsSeason/objectM.js
@@ -0,0 +1,14 @@
+let person1 = {
+    firstName : "Ash",
+    lastName : "Sharma"
+}
+let person2 = {
+    firstName : "Sheena",
+    lastName : "Bajaj"
+}
+let printName = function(city , country){
+    console.log(this.firstName + " " + this.lastName + " " + city + "from " + country)
+}
+
+printName.call(person1,"Pune" , "India")
+printName.call(person2,"Bengaluru" , "India")
