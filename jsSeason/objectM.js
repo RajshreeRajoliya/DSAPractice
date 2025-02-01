f993902427c1e572c9134376ed5e6d6a929ed3ec
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2025-02-01 14:26:00 +0530

    apply

--- jsSeason/objectM.js
+++ jsSeason/objectM.js
@@ -10,5 +10,8 @@ let printName = function(city , country){
     console.log(this.firstName + " " + this.lastName + " " + city + "from " + country)
 }
 
-printName.call(person1,"Pune" , "India")
-printName.call(person2,"Bengaluru" , "India")
+// printName.call(person1,["Pune" , "India"])
+// printName.call(person2,["Bengaluru" , "India"])
+
+printName.apply(person1,["Pune" , "India"])
+printName.apply(person2,["Bengaluru" , "India"])
